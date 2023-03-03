d835a7d9b7d037b620b0e93351185fef18dbebea
morganp3 <morganp3@msu.edu>
2023-03-02 20:40:25 -0500

    Add new file

---
+++ GameLib/Item.h
@@ -0,0 +1,27 @@
+/**
+ * @file Item.h
+ * @author Parker Morgan
+ *
+ *
+ */
+
+#ifndef PROJECT1_GAMELIB_ITEM_H
+#define PROJECT1_GAMELIB_ITEM_H
+
+class Player
+{
+private:
+
+public:
+	/// Default constructor (disabled)
+	Item() = delete;
+
+	/// Copy constructor (disabled)
+	Item(const Item &) = delete;
+
+	/// Assignment operator
+	void operator=(const Item &) = delete;
+
+};
+
+#endif //PROJECT1_GAMELIB_ITEM_H
