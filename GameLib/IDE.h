1068a6080cb74ac62bd61d2c86f7b47808ef5301
morganp3 <morganp3@msu.edu>
2023-03-02 20:42:32 -0500

    Add new file

---
+++ GameLib/IDE.h
@@ -0,0 +1,27 @@
+/**
+ * @file IDE.h
+ * @author Parker Morgan
+ *
+ *
+ */
+
+#ifndef PROJECT1_GAMELIB_IDE_H
+#define PROJECT1_GAMELIB_IDE_H
+
+class IDE
+{
+private:
+
+public:
+	/// Default constructor (disabled)
+	IDE() = delete;
+
+	/// Copy constructor (disabled)
+	IDE(const IDE &) = delete;
+
+	/// Assignment operator
+	void operator=(const IDE &) = delete;
+
+};
+
+#endif //PROJECT1_GAMELIB_IDE_H
