e53b9bb5e6317606981534d7b637a9f6a4c260e3
pietersf <pietersf@msu.edu>
2023-03-03 09:34:26 -0500

    Upload New File

---
+++ GameLib/Level2.h
@@ -0,0 +1,25 @@
+/**
+ * @file Level2.h
+ *
+ * @author Flora Pieters
+ *
+ * Declaration of Level2 class
+ */
+
+#ifndef PROJECT1_GAMELIB_LEVEL2_H
+#define PROJECT1_GAMELIB_LEVEL2_H
+
+#include "Level.h"
+
+using namespace std;
+
+/**
+ * Class that describes second Level in bug game.
+ */
+class Level2 : public Level
+{
+
+};
+
+
+#endif //PROJECT1_LEVEL2_H
