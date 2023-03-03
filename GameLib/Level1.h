fc399e7c97d7ba3a8792256c051f7525e820c239
pietersf <pietersf@msu.edu>
2023-03-03 09:32:49 -0500

    Upload New File

---
+++ GameLib/Level1.h
@@ -0,0 +1,24 @@
+/**
+ * @file Level1.h
+ *
+ * @author Flora Pieters
+ *
+ * Declaration of Level1 class
+ */
+
+#ifndef PROJECT1_GAMELIB_LEVEL1_H
+#define PROJECT1_GAMELIB_LEVEL1_H
+
+using namespace std;
+
+/**
+ * Class that describes first Level in bug game.
+ */
+class Level1 : public Level
+{
+
+
+};
+
+
+#endif //PROJECT1_GAMELIB_LEVEL1_H
