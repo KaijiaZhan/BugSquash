1046706ac94dee98d04f12be210e56ca2a300adb
pietersf <pietersf@msu.edu>
2023-03-10 10:10:04 -0500

    Add constructor to GarbageBug.cpp

--- GameLib/GarbageBug.cpp
+++ GameLib/GarbageBug.cpp
@@ -1,6 +1,7 @@
 /**
  * @file GarbageBug.cpp
  * @author Kaijia Joanna Zhan
+ * @author Flora Pieters
  *
  * Source file for type GarbageBug
  */
@@ -16,3 +17,13 @@ const wstring GarbageBugSpriteImageName = L"blue-maize-bug.png";
 
 /// The splat image
 const wstring GarbageBugSplatImageName = L"blue-maize-splat.png";
+
+/**
+ * Constructor
+ * @param game Game this bug is a member of
+ */
+GarbageBug::GarbageBug(Game *game) : BugCollection(game)
+{
+    mFishImage = make_unique<wxImage>(GarbageBugSpriteImageName, wxBITMAP_TYPE_ANY);
+    mFishBitmap = make_unique<wxBitmap>(*mFishImage);
+}
