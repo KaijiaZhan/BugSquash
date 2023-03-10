b2daefdb2dd041325d3dbc3ec18b8728f2ebf6b9
pietersf <pietersf@msu.edu>
2023-03-10 10:08:39 -0500

    Add constructor to BugCollection

--- GameLib/BugCollection.cpp
+++ GameLib/BugCollection.cpp
@@ -1,7 +1,15 @@
 /**
  * @file BugCollection.cpp
  * @author rmpal
+ * @author Flora Pieters
  */
 
 #include "pch.h"
 #include "BugCollection.h"
+
+/*
+ * Constructor
+ */
+BugCollection::BugCollection(Game *pGame) {
+
+}
