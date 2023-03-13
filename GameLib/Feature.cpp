6760c0279077881b8a2c6c1ef8ddceb84f88e223
Parker Morgan <morganp3@msu.edu>
2023-03-13 12:55:21 -0400

    Added Draw function (not currently working)

--- GameLib/Feature.cpp
+++ GameLib/Feature.cpp
@@ -1,11 +1,11 @@
 /**
  * @file Feature.cpp
  * @author Courtney Thang
+ * @author Parker Morgan
  */
 
 #include "pch.h"
 #include "Feature.h"
-
 #include <string>
 
 using namespace std;
@@ -13,3 +13,27 @@ using namespace std;
 /// Feature filenames
 const wstring FeatureImage = L"feature.png";
 const wstring FeatureSplat = L"feature-splat.png";
+
+/**
+ * Constructor
+ * @param game Game this bug is a member of
+ */
+Feature::Feature(Game *game)
+{
+	mFeatureImage = make_unique<wxImage>(FeatureSplat, wxBITMAP_TYPE_ANY);
+	mFeatureBitmap = make_unique<wxBitmap>(*mFeatureImage);
+}
+
+/**
+ * Draw this feature
+ * @param dc Device context to draw on
+ */
+//void Feature::Draw(wxDC *dc)
+//{
+//	double wid = mFeatureBitmap->GetWidth();
+//	double hit = mFeatureBitmap->GetHeight();
+//
+//	dc->DrawBitmap(*mFeatureBitmap,
+//				   int(GetX() - wid / 2),
+//				   int(GetY() - hit / 2));
+//}
--- GameLib/Feature.h
+++ GameLib/Feature.h
@@ -1,12 +1,17 @@
 /**
  * @file Feature.h
  * @author Courtney Thang
- *
+ * @author Parker Morgan
  *
  */
 
 #ifndef PROJECT1_GAMELIB_FEATURE_H
 #define PROJECT1_GAMELIB_FEATURE_H
+
+#include "Game.h"
+#include "Item.h"
+#include "ScoreBoard.h"
+#include "GameView.h"
 
 /**
  * Initializes the feature item
@@ -14,7 +19,11 @@
 class Feature
 {
 private:
+	/// The underlying Feature image
+	std::unique_ptr<wxImage> mFeatureImage;
 
+	/// The bitmap we can display for feature
+	std::unique_ptr<wxBitmap> mFeatureBitmap;
 public:
 	/// Default constructor (disabled)
 	Feature() = delete;
@@ -25,6 +34,9 @@ public:
 	/// Assignment operator
 	void operator=(const Feature &) = delete;
 
+	Feature(Game* game);
+
+	//void Draw(wxDC* dc) override;
 };
 
 #endif //PROJECT1_GAMELIB_FEATURE_H
