4088c5d1fe9d8eef113679a0510aad6ff53f96e8
Courtney Thang <thangcou@msu.edu>
2023-03-16 17:47:47 -0400

    Added tests for creating the game; unfinished - still needs other functions made to be done with hittests and populating.

--- Tests/GameTest.cpp
+++ Tests/GameTest.cpp
@@ -1,13 +1,115 @@
 /**
  * @file GameTest.cpp
  * @author joann
+ * @author Courtney Thang
  */
 
 #include <pch.h>
-#include "gtest/gtest.h"
 #include <Game.h>
+#include <RedundancyFly.h>
+#include <FatBug.h>
+#include <Feature.h>
+#include <GarbageBug.h>
+#include <NullBug.h>
+#include <regex>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <wx/filename.h>
+
+#include "gtest/gtest.h"
 
 using namespace std;
+
+class GameTest : public ::testing::Test
+{
+protected:
+	/**
+	 * Create a path to a place to put temporary files
+	 */
+	 wxString TempPath()
+	{
+		 // Create a temporary filename we can use
+		auto path = wxFileName::GetTempDir() + L"/game";
+		if(!wxFileName::DirExists(path))
+		{
+			wxFileName::Mkdir(path);
+		}
+
+		return path;
+	}
+	/**
+	* Read a file into a wstring and return it.
+	* @param filename Name of the file to read
+	* @return File contents
+	*/
+	wstring ReadFile(const wxString &filename)
+	{
+		ifstream t(filename.ToStdString());
+		wstring str((istreambuf_iterator<char>(t)),
+					istreambuf_iterator<char>());
+
+		return str;
+	}
+
+	/**
+	* Test to ensure a game level xml file is empty
+	*/
+	void TestEmpty(wxString filename)
+	{
+		cout << "Temp file: " << filename << endl;
+
+		auto xml = ReadFile(filename);
+		cout << xml << endl;
+
+		ASSERT_TRUE(regex_search(xml, wregex(L"<\\?xml.*\\?>")));
+		//not sure if the next like is correct because no aqua files
+		//ASSERT_TRUE(regex_search(xml, wregex(L"<aqua/>")));
+
+	}
+
+	/**
+	*  Populate a game with three redundancy flies
+	*/
+	void PopulateThreeFlies(Game *game)
+	{
+		auto fly1 = make_shared<RedundancyFly>(game);
+
+		/// Add functions have not been made yet, uncomment when Flora makes
+		//game->Add(fly1);
+		fly1->SetLocation(100, 200);
+
+		auto fly2 = make_shared<RedundancyFly>(game);
+		//game->Add(fly2);
+		fly2->SetLocation(400, 400);
+
+		auto fly3 = make_shared<RedundancyFly>(game);
+		//game->Add(fish3);
+		fly3->SetLocation(600, 100);
+	}
+
+//	void TestThreeFlies(wxString filename)
+//	{
+//		cout << "Temp file: " << filename << endl;
+//
+//		auto xml = ReadFile(filename);
+//		cout << xml << endl;
+//
+//		// Ensure three items
+//		/// aqua?
+//		//ASSERT_TRUE(regex_search(xml, wregex(L"<aqua><item.*<item.*<item.*</aqua>")));
+//
+//		// Ensure the positions are correct
+//		ASSERT_TRUE(regex_search(xml, wregex(L"<item x=\"100\" y=\"200\"")));
+//		ASSERT_TRUE(regex_search(xml, wregex(L"<item x=\"400\" y=\"400\"")));
+//		ASSERT_TRUE(regex_search(xml, wregex(L"<item x=\"600\" y=\"100\"")));
+//
+//		// Ensure the types are correct
+//		ASSERT_TRUE(regex_search(xml,
+//								 wregex(
+//									 L"<aqua><item.* type=\"beta\"/><item.* type=\"beta\"/><item.* type=\"beta\"/></aqua>")));
+//	}
+};
 
 TEST(GameTest, Construct){
 	Game game;
@@ -16,27 +118,30 @@ TEST(GameTest, Construct){
 TEST(BugTest, HitTest) {
 //	// Create a bug to test
 //	Game game;
-//	FishBeta bug(&aquarium);
 //
-//	// Give it a location
-//	// Always make the numbers different, in case they are mixed up
-//	bug.SetLocation(100, 200);
+//	ASSERT_EQ(game.HitTest(100, 200), nullptr) <<
+//												   L"Testing empty game";
 //
-//	// Center of the fish should be a true
-//	ASSERT_TRUE(bug.HitTest(100, 200));
+//	shared_ptr <RedundancyFly> bug = make_shared<RedundancyFly>(&game);
 //
-//	// Left of the fish
-//	ASSERT_FALSE(bug.HitTest(10, 200));
+//	///game.Add(bug); NEEDS ADD FUNCTION
+//	bug->SetLocation(100, 200);
 //
-//	// Right of the fish
-//	ASSERT_FALSE(bug.HitTest(200, 200));
+//	// Center of the bug should be a true
+//	ASSERT_TRUE(bug->HitTest(100, 200));
 //
-//	// Above the fish
-//	ASSERT_FALSE(bug.HitTest(100, 0));
+//	// Left of the bug
+//	ASSERT_FALSE(bug->HitTest(10, 200));
 //
-//	// Below the fish
-//	ASSERT_FALSE(bug.HitTest(100, 300));
+//	// Right of the bug
+//	ASSERT_FALSE(bug->HitTest(200, 200));
 //
-//	// On a fish transparent pixel
-//	ASSERT_FALSE(bug.HitTest(100 - 125/2 + 17, 200 - 117/2 + 16));
+//	// Above the bug
+//	ASSERT_FALSE(bug->HitTest(100, 0));
+//
+//	// Below the bug
+//	ASSERT_FALSE(bug->HitTest(100, 300));
+//
+//	// On a bug transparent pixel
+//	ASSERT_FALSE(bug->HitTest(100 - 125/2 + 17, 200 - 117/2 + 16));
 }
