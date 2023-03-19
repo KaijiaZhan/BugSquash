f92161acfe0029a72f93e7af25b0cf3fcd75c217
Courtney Thang <thangcou@msu.edu>
2023-03-19 18:08:12 -0400

    Uncommented GarbageBug hittest.

--- Tests/GarbageBugTest.cpp
+++ Tests/GarbageBugTest.cpp
@@ -12,38 +12,35 @@
 
 TEST(GarbageBugTest, Construct){
 	Game game;
-	/// needs garbagebug draw function
-//	GarbageBug gbug(&game);
+
+	GarbageBug gbug(&game);
 }
 
-//TEST(GarbageBugTest, HitTest) {
+TEST(GarbageBugTest, HitTest) {
 // Create a fish to test
-//	Game game;
-//	game.Clear();
-//
-//	shared_ptr<Item> gbug = std::make_shared<GarbageBug>(&game);
-//	gbug->SetLocation(100, 200);
-//	game.Add(gbug);
-//
-//
-//
-//	// Center of the fish should be a true
-//	ASSERT_TRUE(gbug->HitTest(100, 200));
-//
-//	// Left of the fish
-//	ASSERT_FALSE(gbug->HitTest(10, 200));
-//
-//	// Right of the fish
-//	ASSERT_FALSE(gbug->HitTest(200, 200));
-//
-//	// Above the fish
-//	ASSERT_FALSE(gbug->HitTest(100, 0));
-//
-//	// Below the fish
-//	ASSERT_FALSE(gbug->HitTest(100, 300));
-//
-//	// On a fish transparent pixel
-//	ASSERT_FALSE(gbug->HitTest(100 - 125/2 + 17, 200 - 117/2 + 16));
+	Game game;
+	game.Clear();
 
+	std::shared_ptr<Item> gbug = std::make_shared<GarbageBug>(&game);
+	gbug->SetLocation(100, 200);
+	game.Add(gbug);
 
-//}
+	// Center of the fish should be a true
+	ASSERT_TRUE(gbug->HitTest(100, 200));
+
+	// Left of the fish
+	ASSERT_FALSE(gbug->HitTest(10, 200));
+
+	// Right of the fish
+	ASSERT_FALSE(gbug->HitTest(200, 200));
+
+	// Above the fish
+	ASSERT_FALSE(gbug->HitTest(100, 0));
+
+	// Below the fish
+	ASSERT_FALSE(gbug->HitTest(100, 300));
+
+	// On a fish transparent pixel
+	ASSERT_FALSE(gbug->HitTest(100 - 125/2 + 17, 200 - 117/2 + 16));
+
+}
