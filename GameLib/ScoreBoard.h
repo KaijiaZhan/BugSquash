b62e3ce0897e8390c010d3998438e3520e5a6bee
pietersf <pietersf@msu.edu>
2023-03-19 21:54:36 -0400

    Add member variables to ScoreBoard.h

--- GameLib/ScoreBoard.h
+++ GameLib/ScoreBoard.h
@@ -23,6 +23,41 @@ private:
 	///Bug missed header for the game
 	int mMissed = 0;
 
+
+    /// Game area in virtual pixels
+    const static int Width = 1250;
+
+    /// Game area height in virtual pixels
+    const static int Height = 1000;
+
+    /// Scale to shrink to when in shrink mode
+    const double ShrinkScale = 0.75;
+
+    /// boolean that sets window size
+    bool mShrinked = true;
+
+    double mXOffset = 0;
+
+    double mYOffset = 0;
+
+    double mScale = 0;
+
+    /// Lable for score font size to use
+    const int LabelSize = 40;
+
+    /// The font color to use
+    const wxColour FontColor = wxColour(0, 200, 200);
+
+    /// Left score X location. The right score is
+    /// the width minus this value.
+    const int LeftScoreX = 150;
+
+    /// Score Y location
+    const int ScoreY = 20;
+
+    /// Score label Y location
+    const int ScoreLabelY = 100;
+
 public:
     /**
 	 * Get fixed score
@@ -41,6 +76,8 @@ public:
 	 * @return missed number
 	 */
     double GetMissed() const { return mMissed; }
+
+	void OnDraw(wxDC *dc, std::shared_ptr<wxGraphicsContext> graphics, int width, int height);
 
 };
 
