99a0d90f84900a663ee0845d9503f7a8a84c74ba
pietersf <pietersf@msu.edu>
2023-03-20 12:08:06 -0400

    Add LeadershipBoard class

---
+++ GameLib/LeadershipBoard.h
@@ -0,0 +1,35 @@
+/**
+ * @file LeadershipBoard.h
+ * @author Flora Pieters
+ *
+ * Initializes the leadership board for the game
+ */
+
+#ifndef PROJECT1_GAMELIB_FEATURE_H
+#define PROJECT1_GAMELIB_FEATURE_H
+
+using namespace std;
+
+/**
+* Initializes the leadership board for the game
+*/
+class LeadershipBoard {
+private:
+    int mScoreFirst = 0;
+    int mScoreSecond = 0;
+    int mScoreThird = 0;
+    int mScoreFourth = 0;
+    int mScoreFifth = 0;
+
+    string mNameFirst;
+    string mNameSecond;
+    string mNameThird;
+    string mNameFourth;
+    string mNameFifth;
+
+public:
+
+};
+
+
+#endif //PROJECT1_GAMELIB_FEATURE_H
