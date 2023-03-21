b6efada1fcc1c7179e714729d5bb1709fbdc45c2
pietersf <pietersf@msu.edu>
2023-03-20 20:31:47 -0400

    Change OnDraw in Game.cpp

--- GameLib/Game.cpp
+++ GameLib/Game.cpp
@@ -77,33 +77,6 @@ void Game::OnDraw(wxDC *dc, std::shared_ptr<wxGraphicsContext> graphics, int wid
 				wxFONTWEIGHT_NORMAL);
 	graphics->SetFont(font, wxColour(0,200,200));
 	//graphics->SetTextForeground(FontColor);
-
-	graphics->DrawText(L"Fixed",  // Text to draw
-				50,     // x coordinate for the left size of the text
-				ScoreLabelY);    // y coordinate for the top of the text
-
-	graphics->DrawText(L"0",  // Text to draw
-				90,     // x coordinate for the left size of the text
-				ScoreLabelY - 50);    // y coordinate for the top of the text
-
-	graphics->DrawText(L"Missed",  // Text to draw
-				450,     // x coordinate for the left size of the text
-				ScoreLabelY);    // y coordinate for the top of the text
-
-	graphics->DrawText(L"0",  // Text to draw
-				490,     // x coordinate for the left size of the text
-				ScoreLabelY - 50);    // y coordinate for the top of the text
-
-	graphics->DrawText(L"Oops",  // Text to draw
-				875,     // x coordinate for the left size of the text
-				ScoreLabelY);    // y coordinate for the top of the text
-
-	graphics->DrawText(L"0",  // Text to draw
-				915,     // x coordinate for the left size of the text
-				ScoreLabelY - 50);    // y coordinate for the top of the text
-
-				// need laptop and bug scaled
-	graphics->PopState();
 }
 
 
