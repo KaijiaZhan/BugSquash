92ea194def0ae6186bf23a402c1519259df847f9
pietersf <pietersf@msu.edu>
2023-03-28 17:56:03 -0400

    Add menu handler for Ok and Close for dialog and add initialize function for CodeWindow

--- GameLib/CodeWindow.cpp
+++ GameLib/CodeWindow.cpp
@@ -15,3 +15,39 @@ wxDialog(parent, -1, title)
 {
 	mCode = code;
 }
+
+/**
+ * Initialize the Dialog.
+ */
+void CodeWindow::Initialize()
+{
+    Create(nullptr,wxID_ANY, L"Bug Squash IDE",wxDefaultPosition, wxSize(600,600 ));
+    auto sizer = new wxBoxSizer( wxVERTICAL );
+    mText = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(20,20), wxTE_MULTILINE);
+    sizer->Add(mText, 1, wxALIGN_CENTER, 6);
+    SetSizerAndFit(sizer);
+    Layout();
+
+    Bind(wxEVT_CLOSE_WINDOW, &CodeWindow::OnClose, this);
+    auto okbutton = new wxButton(this, wxID_ANY,L"Ok", wxPoint(200, 300));
+    okbutton->Bind(wxEVT_BUTTON, &CodeWindow::OnOk, this);
+
+}
+
+/**
+ * Close menu option handlers
+ * @param event
+ */
+void CodeWindow::OnClose(wxCloseEvent& event)
+{
+
+}
+
+/**
+ * Ok menu option handler
+ * @param event
+ */
+void CodeWindow::OnOk(wxCommandEvent& event)
+{
+
+}
